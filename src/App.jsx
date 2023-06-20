c890696cc9006047827b84faf4552dae2aba2a89
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-06-20 13:26:32 +1000

    tailwind,router,axios and logo imported and theme configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="flex items-center w-full flex-wrap gap-5 mx-auto flex-grow">
+        <button className="text-5xl shadow-large bg-logoPurple text-white px-10 rounded-lg ">
+          Hello world
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className="text-5xl shadow-large bg-softWhite text-white px-10 rounded-lg ">
+          Hello world
+        </button>
+        <button className="text-5xl shadow-large bg-softCream text-white px-10 rounded-lg ">
+          Hello world
+        </button>
+        <button className="text-5xl shadow-large bg-purple text-white px-10 rounded-lg ">
+          Hello world
+        </button>
+        <button className="text-5xl shadow-large bg-green text-white px-10 rounded-lg ">
+          Hello world
+        </button>
+        <button className="text-5xl shadow-large bg-yellow text-white px-10 rounded-lg ">
+          Hello world
+        </button>
+        <button className="text-5xl shadow-large bg-darkBlue text-white px-10 rounded-lg ">
+          Hello world
+        </button>
+        <button className="text-5xl shadow-large bg-redPink text-white px-10 rounded-lg ">
+          Hello world
+        </button>
+
+        <button className="text-5xl shadow-large bg-sweetOrange text-white px-10 rounded-lg ">
+          Hello world
+        </button>
+        <button className="text-5xl shadow-large bg-whiteOrange text-white px-10 rounded-lg ">
+          Hello world
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "450px",
+      md: "800px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+
+    fontSize: {
+      sm: ".5rem",
+      base: "1.rem",
+      lg: "1.5rem",
+      xl: "2rem",
+      "2xl": "2.5rem",
+      "3xl": "3rem",
+      "4xl": "3.5rem",
+      "5xl": "4rem",
+    },
+    extend: {
+      colors: {
+        darkPurple: "#764abc",
+        softWhite: "#edebe0",
+        softCream: "#e2c8a1",
+        purple: "#8E7CC3",
+        green: "#38761D",
+        yellow: "#F3CD00",
+        darkBlue: "#00234b",
+        redPink: "#e73b73",
+        logoPurple: "#393f57",
+        sweetOrange: "#f59e7c",
+        whiteOrange: "#eddad1",
+      },
+      boxShadow: {
+        regular:
+          " rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;",
+        medium:
+          "box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;",
+        large:
+          "box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;",
+      },
+    },
+  },
+  plugins: [],
+};
