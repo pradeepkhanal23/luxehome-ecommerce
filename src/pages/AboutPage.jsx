b3da1f6db06b682657b781ee9f18872aca06c32c
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-06-28 19:47:57 +1000

    about page height issue in over medium screen adjusted, final UI design before context API

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,8 +5,8 @@ const AboutPage = () => {
   return (
     <>
       <PageHero title="about" />
-      <section className="flex flex-col justify-center  items-center  max-w-[1800px] my-3 mx-auto p-2 gap-[2rem] md:flex-row md:items-center md:justify-center  ">
-        <div className="w-9/10 h-9/10 self-start ">
+      <section className="flex flex-col justify-center h-full items-center  max-w-[1800px] my-3 mx-auto p-2 gap-[2rem] md:flex-row md:items-center md:justify-center md:h-[calc(100vh-9rem)] ">
+        <div className="14/15 h-14/15 self-start ">
           <img
             src={HeroImage}
             alt="hero-img"
