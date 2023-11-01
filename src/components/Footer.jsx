e8207c14e978fcb307ed78e6a9c0ed2fa7d7861b
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-11-01 13:29:36 +1100

    footer issue fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
       <div className="w-full h-14 text-center flex flex-col items-center justify-center">
         <h5>
           &copy; {new Date().getFullYear()}
-          <span className="font-bold uppercase drop-shadow-2xl text-sweetOrange">
-            LuxeHome
+          <span className="font-bold uppercase drop-shadow-2xl text-[#FFD6A5]">
+            {``} LuxeHome
           </span>
         </h5>
         <h5>All Rights Reserved</h5>
