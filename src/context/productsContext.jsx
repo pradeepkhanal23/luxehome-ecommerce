e84be8db656eea08aa87206f470b8acc8ea4ad18
Pradeep Khanal <pradeepkhanal642@gmail.com>
2025-02-28 14:23:08 +1100

    app url changed to the render hosted url

--- src/context/productsContext.jsx
+++ src/context/productsContext.jsx
@@ -46,7 +46,6 @@ export const ProductsProvider = ({ children }) => {
       const response = await axios(url);
 
       const products = response.data.data;
-      console.log(products);
 
       dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
     } catch (error) {
